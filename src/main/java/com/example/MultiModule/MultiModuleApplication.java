0cd378b9e1bc0931617522ebd5310ca80a408005
Rain569 <569353115@qq.com>
2018-05-20 17:32:41 +0800

    注释

---
+++ src/main/java/com/example/MultiModule/MultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.MultiModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleApplication {发顺丰
+烦死哒
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleApplication.class, args);
+	}
+}
